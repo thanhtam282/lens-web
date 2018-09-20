b37ce254dea0ea7bd41d35b243a90eba419ed1f2
Nguyen Thanh Tam <ntt.hcm.vn@gmail.com>
2018-09-20 22:31:24 +0700

    first commit

---
+++ src/scripts/main.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+	$(".thanhtam-slider .owl-carousel").owlCarousel({
+		items: 1,
+		nav: false,
+		dots: false,
+		navText: ['<i class="fa fa-chevron-left"></i>', '<i class="fa fa-chevron-right"></i>'],
+		loop:true
+	});
+	$(".thanhtam-seller .owl-carousel").owlCarousel({
+		items: 1,
+		nav: true,
+		dots: false,
+		navText: ['<i class="fa fa-chevron-left"></i>', '<i class="fa fa-chevron-right"></i>'],
+		loop:true
+	});
+	$(".thanhtam-piece  .owl-carousel").owlCarousel({
+		items: 4,
+		nav: true,
+		dots: false,
+		navText: ['<i class="fa fa-chevron-left"></i>', '<i class="fa fa-chevron-right"></i>'],
+		loop:true,
+		responsive: {
+			// breakpoint from 480 up
+			360: {
+				items: 2,
+			},
+			480: {
+				items: 2,
+			},
+			640: {
+				items: 3,
+			},
+			// breakpoint from 768 up
+			768: {
+				items: 4,
+			},
+			// breakpoint from 992 up
+			992: {
+				items: 4,
+			}
+		}
+	});
+});
